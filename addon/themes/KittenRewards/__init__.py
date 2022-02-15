0faf74f79eccebf4f09de709511e9b03947c8def
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-02-15 13:25:11 +0900

    resource_url use __file__ to get theme name Manually providing theme name as string is bad as there is now two things to modify when changing theme name. With this commit, you can change theme name by changing only folder name

--- addon/themes/KittenRewards/__init__.py
+++ addon/themes/KittenRewards/__init__.py
@@ -6,22 +6,27 @@ from aqt.webview import AnkiWebView
 
 from .. import resourse_url, triggers, events
 
+
 def random_image() -> str:
     """Returns random cat image url"""
     dir = Path(__file__).parent / "images"
     files = list(dir.glob("**/*"))
     file = random.choice(files)
-    rel_path = os.path.relpath(str(file), str(dir))
-    return resourse_url(f"KittenRewards/images/{rel_path}")
+    rel_path = file.relative_to(dir)
+    return resourse_url(__file__, f"images/{str(rel_path)}")
+
 
 def on_congrats(web: AnkiWebView):
     """Insert cat image onto Congrats page"""
     image_url = random_image()
-    html = """
+    html = (
+        """
 <div id="cat-container">
     <img id="cat-image" src="%s">
 </div>
-    """ % image_url
+    """
+        % image_url
+    )
     style = """
 #cat-container {
     display: flex;
@@ -35,5 +40,6 @@ def on_congrats(web: AnkiWebView):
 
     events.add_style(web, style)
     events.add_html(web, html, to="top")
+
 
 triggers.congrats_page(on_congrats)
--- addon/themes/__init__.py
+++ addon/themes/__init__.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+
 from aqt import mw
 from . import triggers
 
 
-def resourse_url(file: str) -> str:
-    return f"/_addons/{mw.addonManager.addonFromModule(__name__)}/themes/{file}"
+def resourse_url(file: str, resource: str) -> str:
+    """
+    - file: pass __file__
+    - resource: relative path from its theme directory
+    """
+    script_path = Path(file)
+    theme = script_path.relative_to(Path(__file__).parent).parts[0]
+    return f"/_addons/{mw.addonManager.addonFromModule(__name__)}/themes/{theme}/{resource}"
 
 
 mw.addonManager.setWebExports(__name__, r"themes/.*")
