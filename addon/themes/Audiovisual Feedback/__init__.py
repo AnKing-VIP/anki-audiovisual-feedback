962c2928c06b78b00e272314acde13fe5db0aed9
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-02-19 16:15:53 +0900

    slightly modify colors The new colors look much better in night mode

--- addon/themes/Audiovisual Feedback/__init__.py
+++ addon/themes/Audiovisual Feedback/__init__.py
@@ -1,9 +1,10 @@
-from typing import Literal
+from typing import Literal, Tuple
 from pathlib import Path
 
 from aqt.reviewer import Reviewer
 from anki.cards import Card
 from aqt.webview import WebContent
+from aqt import gui_hooks
 
 from .. import resource_url, theme_name, triggers, events, Ease
 from .config import conf
@@ -39,6 +40,30 @@ def on_reviewer_page(web: WebContent):
     web.js.append(resource_url(__file__, "script.js"))
 
 
+def colorful_span(orig: str, color: str) -> str:
+    return f"<span style='color: {color}'>{orig}</span>"
+
+
+def color_answer_button(
+    buttons_tuple: Tuple[Tuple[int, str], ...], reviewer: Reviewer, card: Card
+) -> Tuple[Tuple[int, str], ...]:
+    buttons = []
+    for button in buttons_tuple:
+        ease = Ease.from_num(button[0], len(buttons_tuple))
+        if ease == Ease.Again:
+            html = colorful_span(button[1], "#f00")
+        elif ease == Ease.Hard:
+            html = colorful_span(button[1], "#f90")
+        elif ease == Ease.Good:
+            html = colorful_span(button[1], "#0e0")
+        else:
+            html = colorful_span(button[1], "#09f")
+        buttons.append((button[0], html))
+    return tuple(buttons)
+
+
 triggers.reviewer_page(on_reviewer_page)
 triggers.answer_card(on_answer_card)
 events.will_use_audio_player()
+
+gui_hooks.reviewer_will_init_answer_buttons.append(color_answer_button)
