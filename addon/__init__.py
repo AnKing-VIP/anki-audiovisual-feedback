454c3bc1df484d1d3d13680ce69d3132273f3ab2
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-02-24 12:39:38 +0900

    run theme code

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,3 +1,20 @@
 from . import firstrun
-from . import config
+from .config import conf
 from . import main
+
+from importlib import import_module
+from aqt.utils import showWarning
+
+theme = conf["theme"]
+if "." not in theme:
+    import_module(f"{__name__}.user_files.themes.{theme}")  # Works with whitespace
+else:
+    showWarning(
+        (
+            "<h3>Gamify Anki Add-on Error</h3>"
+            "Full stop (.) is not aloowed in theme name.<br />"
+            f"current theme: {conf['theme']}"
+        ),
+        title="Gamify Anki Addon Error",
+        textFormat="rich",
+    )
