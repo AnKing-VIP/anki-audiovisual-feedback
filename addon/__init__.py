a36ce7f49afae3b6dfb143583ebf245b9bf12100
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-03-11 15:31:00 +0900

    remove python code in themes

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,20 +1,2 @@
 from . import firstrun
-from .config import conf
 from . import main
-
-from importlib import import_module
-from aqt.utils import showWarning
-
-theme = conf["theme"]
-if "." not in theme:
-    import_module(f"{__name__}.user_files.themes.{theme}")  # Works with whitespace
-else:
-    showWarning(
-        (
-            "<h3>Gamify Anki Add-on Error</h3>"
-            "Full stop (.) is not aloowed in theme name.<br />"
-            f"current theme: {conf['theme']}"
-        ),
-        title="Gamify Anki Addon Error",
-        textFormat="rich",
-    )
