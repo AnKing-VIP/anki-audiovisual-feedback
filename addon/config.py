d7afe45e02c1775b53d7e406d427df927b8380c8
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-04-03 17:59:31 +0900

    add open theme folder button to config

--- addon/config.py
+++ addon/config.py
@@ -1,13 +1,22 @@
 from typing import List
 from pathlib import Path
 
+from aqt.qt import QPushButton
+from aqt.utils import openFolder
+
 from .ankiaddonconfig import ConfigManager, ConfigWindow
+
+THEMES_DIR = Path(__file__).parent / "user_files" / "themes"
+
+
+def open_theme_dir() -> None:
+    theme_dir = THEMES_DIR / conf["theme"]
+    openFolder(theme_dir)
 
 
 def get_themes() -> List[str]:
-    themes_dir = Path(__file__).parent / "user_files" / "themes"
     themes = []
-    for child in themes_dir.iterdir():
+    for child in THEMES_DIR.iterdir():
         if child.is_dir():
             themes.append(child.name)
     return themes
@@ -27,6 +36,17 @@ def general_tab(conf_window: ConfigWindow) -> None:
     )
     tab.checkbox("review_effect", "During review ")
     tab.checkbox("congrats_effect", "On completing deck ")
+
+    btn_lay = tab.hlayout()
+
+    btn = QPushButton("Open Theme Folder")
+    btn.clicked.connect(lambda _: open_theme_dir())
+    btn.setToolTip(
+        "You can customize themes by modifying files in the theme directory."
+    )
+    btn_lay.addWidget(btn)
+    btn_lay.stretch()
+
     tab.stretch()
 
 
