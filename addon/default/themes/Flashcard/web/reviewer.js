34eb6fa200bfbe70e7795fae0739b629dc0c2a51
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-04-25 17:36:06 +0900

    flashcard: show visual indicator on all sides

--- addon/default/themes/Flashcard/web/reviewer.js
+++ addon/default/themes/Flashcard/web/reviewer.js
@@ -1,25 +1,38 @@
 (() => {
-    const onLoad = () => {
+
+    const visualFeedbackDiv = () => {
         const div = document.createElement("div")
-        div.id = "visualFeedback"
+        div.classList.add("visualFeedback")
         document.body.appendChild(div)
+        return div
+    }
+
+    const onLoad = () => {
+        for (const pos of ["top", "bottom", "left", "right"]) {
+            const div = visualFeedbackDiv();
+            div.classList.add(pos);
+        }
     }
 
     document.readyState === "complete" ? onLoad() : window.addEventListener("load", onLoad)
 
-    
+
     let timeout = null
-    
+
     // ease: string "again" / "hard" / "good" / "easy"
     window.showVisualFeedback = (ease) => {
-        const elem = document.getElementById("visualFeedback")
+        const elems = document.getElementsByClassName("visualFeedback")
         if (timeout) {
             clearTimeout(timeout)
         }
-        elem.classList.add(ease)
+        for (elem of elems) {
+            elem.classList.add(ease)
+        }
 
         timeout = setTimeout((c) => {
-            elem.classList.remove(c)
+            for (elem of elems) {
+                elem.classList.remove(c)
+            }
         }, 300, ease)
     }
 })()
